2f24e3b6764769569248c5854ffa6b499736ff23
AnahAntonerrie <andrianahanah31@gmail.com>
2026-07-08 12:00:50 +0300

    fix: correct entity imports and remove undefined service method calls in health controllers

--- src/main/java/com/library/hei/endpoint/rest/controller/health/AuthorController.java
+++ src/main/java/com/library/hei/endpoint/rest/controller/health/AuthorController.java
@@ -1,53 +1,45 @@
 package com.library.hei.endpoint.rest.controller.health;
 
-import com.library.hei.entity.Author;
+import com.library.hei.model.entity.Author;
 import com.library.hei.service.AuthorService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/authors")
 public class AuthorController {
 
-  private final AuthorService authorService;
+    private final AuthorService authorService;
 
-  public AuthorController(AuthorService authorService) {
-    this.authorService = authorService;
-  }
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Author createAuthor(@RequestBody Author author) {
-    return authorService.createAuthor(author);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Author createAuthor(@RequestBody Author author) {
+        return authorService.crupdate(null, author);
+    }
 
-  @GetMapping("/{id}")
-  public Author getAuthorById(@PathVariable Long id) {
-    return authorService
-        .getAuthorById(id)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found"));
-  }
+    @GetMapping("/{id}")
+    public Author getAuthorById(@PathVariable String id) {
+        return authorService.getById(id);
+    }
 
-  @GetMapping
-  public List<Author> getAllAuthors() {
-    return authorService.getAllAuthors();
-  }
+    @GetMapping
+    public List<Author> getAllAuthors() {
+        return authorService.getAll();
+    }
 
-  @GetMapping("/search")
-  public List<Author> searchAuthorsByLastName(@RequestParam String lastName) {
-    return authorService.searchByLastName(lastName);
-  }
+    @PutMapping("/{id}")
+    public Author updateAuthor(@PathVariable String id, @RequestBody Author author) {
+        return authorService.crupdate(id, author);
+    }
 
-  @PutMapping("/{id}")
-  public Author updateAuthor(@PathVariable Long id, @RequestBody Author author) {
-    return authorService.updateAuthor(id, author);
-  }
-
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteAuthor(@PathVariable Long id) {
-    authorService.deleteAuthor(id);
-  }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAuthor(@PathVariable String id) {
+        authorService.delete(id);
+    }
 }
--- src/main/java/com/library/hei/endpoint/rest/controller/health/GenreController.java
+++ src/main/java/com/library/hei/endpoint/rest/controller/health/GenreController.java
@@ -1,55 +1,45 @@
 package com.library.hei.endpoint.rest.controller.health;
 
-import com.library.hei.entity.Genre;
+import com.library.hei.model.entity.Genre;
 import com.library.hei.service.GenreService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/genres")
 public class GenreController {
 
-  private final GenreService genreService;
+    private final GenreService genreService;
 
-  public GenreController(GenreService genreService) {
-    this.genreService = genreService;
-  }
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Genre createGenre(@RequestBody Genre genre) {
-    return genreService.createGenre(genre);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Genre createGenre(@RequestBody Genre genre) {
+        return genreService.crupdate(null, genre);
+    }
 
-  @GetMapping("/{id}")
-  public Genre getGenreById(@PathVariable Long id) {
-    return genreService
-        .getGenreById(id)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Genre not found"));
-  }
+    @GetMapping("/{id}")
+    public Genre getGenreById(@PathVariable String id) {
+        return genreService.getById(id);
+    }
 
-  @GetMapping
-  public List<Genre> getAllGenres() {
-    return genreService.getAllGenres();
-  }
+    @GetMapping
+    public List<Genre> getAllGenres() {
+        return genreService.getAll();
+    }
 
-  @GetMapping("/search")
-  public Genre getGenreByName(@RequestParam String name) {
-    return genreService
-        .getGenreByName(name)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Genre not found"));
-  }
+    @PutMapping("/{id}")
+    public Genre updateGenre(@PathVariable String id, @RequestBody Genre genre) {
+        return genreService.crupdate(id, genre);
+    }
 
-  @PutMapping("/{id}")
-  public Genre updateGenre(@PathVariable Long id, @RequestBody Genre genre) {
-    return genreService.updateGenre(id, genre);
-  }
-
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteGenre(@PathVariable Long id) {
-    genreService.deleteGenre(id);
-  }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGenre(@PathVariable String id) {
+        genreService.delete(id);
+    }
 }
